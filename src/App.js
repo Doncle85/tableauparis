fac9a5a0682b2ff2f1fb3c14c28a64d897bef25f
az02754 <clement.c.malarme@renault.com>
2020-10-15 14:00:24 +0200

    Create project. add directory component/containers/asset. add bootstrap and dark theme bootswatch.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+   <div className="container">
+     <h1>Titre</h1>
+       <div>Paris</div>
+       <button>Ajouter</button>
+   </div>
+  );
+}
+
+export default App;
